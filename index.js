e9437e3706432996a185f4e8fd695dae9864cef4
surangana thapa <shilpithapa233@gmail.com>
2025-07-15 14:39:23 +0545

    corrected the error and completed the game

--- index.js
+++ index.js
@@ -29,12 +29,12 @@ function changeDirection(e) // here, e=event
 
 }
 function gameloop(){
-    const head ={x:snake[0].x+direction.x , y:snake[0].y+direction.y}
+    const head ={x: snake[0].x + direction.x , y: snake[0].y + direction.y}
     //here, x: means x cooridnate 
     // snake[0].x bhaneko snake ko head ko position in x (current) coordinate
     //direction.x bhaneko snake ko head ahile kun (current) direction ma cha
     //yo position ra direction of addition bhako cha same for y coordinate
-    if(head.x<0 || head.x>=containerSize || head.y<0 || head.y>= containerSize)
+    if(head.x<0 || head.x >= containerSize || head.y < 0 || head.y>= containerSize)
     {
         alert("Game Over")
         clearInterval(gameInterval)
@@ -50,18 +50,35 @@ function gameloop(){
         snake.pop()
         //snake le food nakhaye samma new food generate hudaina(meaning kehi pani hudaina)
     }
+    draw()
 }
 function draw() // yo function le snake le food khaye pachi segment badhaune kaam garcha
 {
     container.innerHTML ='' //'' -> empty
     snake.forEach(segment =>{
-       const SnakePart = document.createElement("div")
+       const snakePart = document.createElement("div")
         snakePart.classList.add('snake')
         //mathi ko 2 ota line ko code le dynamically snake bhanne div class banaucha
-        snakePart.style.left = segment.x + "px"
+        snakePart.style.left = segment.x + "px"//snake ta segment ma aaucha so segment.x gareko
         snakePart.style.top = segment.y + "px"
+        snakePart.style.position='absolute'
         container.appendChild(snakePart)
     })
-        
+    const foodPart = document.createElement("div")
+        foodPart.classList.add('food')
+        //mathi ko 2 ota line ko code le dynamically food bhanne div class banaucha
+        foodPart.style.left = food.x + "px"
+        foodPart.style.top = food.y + "px"
+        foodPart.style.position ='absolute'
+        container.appendChild(foodPart)
     }
+
+    function generateFood(){
+        const x = Math.floor(Math.random()*(containerSize / gridsize)) * gridsize
+        const y = Math.floor(Math.random()*(containerSize / gridsize)) * gridsize
+        return{x,y}
+        ///here, Math.random creates random no.
+        ///here, Math.floor helps to round off (e.g:5.23->5)
+    }
+    draw()
 
